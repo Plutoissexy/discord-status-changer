9e8ffb7d872b2dcf50ed733e9ae1f8731fab8b1e
Cipher <admin@cipherioapi.xyz>
2022-10-16 15:21:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import json
 from discord.ext import commands
 
 token = input("Enter your token here: ")
+text = input("Enter the text for your status: ")
+emoji = input("Enter in emoji format your emoji: ")
 
 url = 'https://discord.com/api/v9/users/@me/settings'
 
-res = requests.patch(url, data=json.dumps({"custom_status":{"text":"glock is hot","emoji_name":"😈"}}), headers={"authorization": token, "content-type": "application/json"})
+res = requests.patch(url, data=json.dumps({"custom_status":{"text":f"{text}","emoji_name":f"{emoji}"}}), headers={"authorization": token, "content-type": "application/json"})
 
 print(res.text)
